a2126e001a552252f46a63e7b6df2a55861cdc64
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-04-13 21:53:45 +0530

    13-April-2026 Learn Spring JDBC through a Course management project say it minor

---
+++ Jdbc-Project-Minor/Jdbc-Project-Minor/src/main/java/com/course/management/Jdbc_Project_Minor/JdbcProjectMinorApplication.java
@@ -0,0 +1,13 @@
+package com.course.management.Jdbc_Project_Minor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcProjectMinorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcProjectMinorApplication.class, args);
+	}
+
+}
